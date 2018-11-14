d9b532359ea78ec846c2cf288bed32f00404794a
Violet Cheng <vcheng@pivotal.io>
2018-11-14 03:24:21 +0000

    fix bug in random sql

--- run_random_sql.sh
+++ run_random_sql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     echo -e "\nUsage:\t $0 HOSTNAME START END REPEAT_TIME\n"
     exit 1
 fi
@@ -7,7 +7,8 @@ DIFF=$(($3-$2+1))
 RANDOM=$$
 for i in `seq $4`
 do
-    R=$(($(($RANDOM%$DIFF))+$1))
+    R=$(($(($RANDOM%$DIFF))+$2))
     echo $R
-    psql -h $1 -U gpadmin -f ./tpch_sqls/$R.sql -d tpch_parquet_20gpn_snappy_part_random_gpadmin
+    new_name=`printf "./tpch_sqls/%0.2i.sql\n" $R`
+    psql -h $1 -U gpadmin -f $new_name -d tpch_parquet_20gpn_snappy_part_random_gpadmin
 done
