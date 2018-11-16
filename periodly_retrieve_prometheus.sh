b20b00321089a43362303be17d8a46e7b4ac1832
Violet Cheng <vcheng@pivotal.io>
2018-11-16 16:00:41 +0800

    call to db prometheus

--- periodly_retrieve_prometheus.sh
+++ periodly_retrieve_prometheus.sh
@@ -5,6 +5,6 @@ sed "s/TIME_HOLDER/$DATE/g" log_template.json > log.json
 es2csv -q '*' -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i prometheus-$1 -o /tmp/prometheus.csv -k
 es2csv -q @'./log.json' -r -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i logstash-`date +%Y.%m.%d` -o /tmp/api-log-0.csv -k
 es2csv -q @'./log.json' -r -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i logstash-`date -d '-1 day' +%Y.%m.%d` -o /tmp/api-log-1.csv -k
-# delete the prometheus database
+
 ./delete.sh prometheus-$1
-#call python program to handle the files
+./to_db_prometheus.sh
--- to_db_prometheus.sh
+++ to_db_prometheus.sh
@@ -1,5 +1,4 @@
-psql -d hawq-recommend -f create_prometheus.sql
-cp $1/prometheus.csv $1/prometheus-origin.csv
-sed -i '' '/scrape/d' $1/prometheus.csv
-sed -i '' '/,linux,/d' $1/prometheus.csv
-psql -d hawq-recommend -c "copy k8s_prometheus_metrics from '$1/prometheus.csv' CSV HEADER"
+sed -i '' '/scrape/d' prometheus.csv
+sed -i '' '/,linux,/d' prometheus.csv
+psql -d hawq-recommend -c "copy k8s_prometheus_metrics from './prometheus.csv' CSV HEADER"
+rm prometheus.csv
