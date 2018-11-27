bba23ed2929f66ec6924f79cac4e8178494e571b
Violet Cheng <vcheng@pivotal.io>
2018-11-27 06:35:31 +0000

    add data size

--- run_random_sql.sh
+++ run_random_sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl get hawqresourcepool -o yaml > config.yaml
-if [ $# -ne 4 ]; then
-    echo -e "\nUsage:\t $0 HOSTNAME START END REPEAT_TIME\n"
+if [ $# -ne 5 ]; then
+    echo -e "\nUsage:\t $0 HOSTNAME START END REPEAT_TIME DATA_SIZE\n"
     exit 1
 fi
 DIFF=$(($3-$2+1))
@@ -11,5 +11,9 @@ do
     R=$(($(($RANDOM%$DIFF))+$2))
     echo $R
     new_name=`printf "./tpch_sqls/%0.2i.sql\n" $R`
-    psql -h $1 -U gpadmin -f $new_name -d tpch_parquet_20gpn_snappy_part_random_gpadmin
+    command="psql -h $1 -U gpadmin -f $new_name -d tpch_parquet_$5gpn_snappy_part_random_gpadmin >> ./sql.log 2>&1"
+    echo "$command\n"
+
+    psql -h $1 -U gpadmin -f $new_name -d tpch_parquet_$5gpn_snappy_part_random_gpadmin >> ./sql.log 2>&1
+    sleep 5
 done
