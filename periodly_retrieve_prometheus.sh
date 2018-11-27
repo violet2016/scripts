cfadeb73fe4fb54729a2dc680f327f37974003f9
Violet Cheng <vcheng@pivotal.io>
2018-11-27 14:37:13 +0800

    add -1 hour in log retrieve

--- periodly_retrieve_prometheus.sh
+++ periodly_retrieve_prometheus.sh
@@ -2,7 +2,7 @@
 BASEDIR=$(dirname "$0")
 pushd $BASEDIR
 prometheus_db_suffix=$1
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
+DATE=`date -d '-1 hour' '+%Y-%m-%d %H:%M:%S'`
 sed "s/TIME_HOLDER/$DATE/g" ./log_template.json > ./log.json
 es2csv -q '*' -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i prometheus-$1 -o /tmp/prometheus.csv -k
 es2csv -q @'./log.json' -r -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i logstash-`date +%Y.%m.%d` -o /tmp/api-log-0.csv -k
@@ -12,4 +12,5 @@ es2csv -q @'./log.json' -r -u http://localhost:8001/api/v1/namespaces/hawq-monit
 set -e
 ./to_db_prometheus.sh /tmp/prometheus.csv
 rm /tmp/prometheus.csv
+rm ./log.json
 popd
