bdf15b9e778a116262acf9e66fbd01630259c775
Violet Cheng <vcheng@pivotal.io>
2018-11-29 15:20:33 +0800

    add ip and pool

--- periodly_retrieve_prometheus.sh
+++ periodly_retrieve_prometheus.sh
@@ -3,6 +3,8 @@ BASEDIR=$(dirname "$0")
 pushd $BASEDIR
 prometheus_db_suffix=$1
 DATE=`date -d '-1 hour' '+%Y-%m-%d %H:%M:%S'`
+kubectl get pods -o wide > ./ip
+kubectl describe hawqresourcepool hawq-resource-pool > ./resourcepool.yaml
 sed "s/TIME_HOLDER/$DATE/g" ./log_template.json > ./log.json
 es2csv -q '*' -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i prometheus-$1 -o /tmp/prometheus.csv -k
 es2csv -q @'./log.json' -r -u http://localhost:8001/api/v1/namespaces/hawq-monitoring/services/elasticsearch-logging/proxy -i logstash-`date +%Y.%m.%d` -o /tmp/api-log-0.csv -k
