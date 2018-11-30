fe25426845499bc22f093bdd4133053900bfe3e5
Violet Cheng <vcheng@pivotal.io>
2018-11-30 10:28:07 +0800

    add limit except

--- python/retrieve_ip.py
+++ python/retrieve_ip.py
@@ -20,9 +20,12 @@ def get_segment_list(ip_filename, yaml_file):
                 pod_list[name]['limit_cpu'] = groups['Group Resource Limit']['Cpu']
                 pod_list[name]['limit_mem'] = groups['Group Resource Limit']['Memory']
                 pod_list[name]['limit_storage'] = groups['Group Resource Limit']['Ephemeral Storage']
-                req_name = 'Group Resource Request'
-                if groups[req_name] is None:
-                    req_name = 'Group Resource Limit'
+                req_name = 'Group Resource Limit'
+                try:
+                    if groups['Group Resource Request'] is not None:
+                        req_name = 'Group Resource Request'
+                except:
+                    print("no limit in", name)
                 pod_list[name]['req_cpu'] = groups[req_name]['Cpu']
                 pod_list[name]['req_mem'] = groups[req_name]['Memory']
                 pod_list[name]['req_storage'] = groups[req_name]['Ephemeral Storage']
