b528f7ed44b25f037e79c87845bd32039e4943ee
Violet Cheng <vcheng@pivotal.io>
2018-12-04 13:16:38 +0800

    fix datetime name

--- create_index_for_day.sh
+++ create_index_for_day.sh
@@ -14,11 +14,11 @@ create index k8s_prometheus_metrics_sample_time_$DATE_INNAME on k8s_prometheus_m
 CREATE OR REPLACE FUNCTION k8s_prometheus_insert_trigger()
 RETURNS TRIGGER AS \$\$
 BEGIN
-    IF ( NEW.logdate >= DATE '$DATE_TODAY' AND
-         NEW.logdate < DATE '$DATE_TOMORROW' ) THEN
+    IF ( NEW.sample_time >= DATE '$DATE_TODAY' AND
+         NEW.sample_time < DATE '$DATE_TOMORROW' ) THEN
         INSERT INTO k8s_prometheus_metrics_d_$DATE_INNAME_TODAY VALUES (NEW.*);
-    ELSIF ( NEW.logdate >= DATE '$DATE_TOMORROW' AND
-            NEW.logdate < DATE '$DATE_INSQL_N' ) THEN
+    ELSIF ( NEW.sample_time >= DATE '$DATE_TOMORROW' AND
+            NEW.sample_time < DATE '$DATE_INSQL_N' ) THEN
         INSERT INTO k8s_prometheus_metrics_d_$DATE_INNAME  VALUES (NEW.*);
     ELSE
         RAISE EXCEPTION 'Date out of range.  Fix the k8s_prometheus_insert_trigger() function!';
