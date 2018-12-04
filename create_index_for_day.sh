af5133df2a07c9f24114c77e4353cb8dc2af4a04
Violet Cheng <vcheng@pivotal.io>
2018-12-04 02:37:58 +0000

    fix permission

--- create_index_for_day.sh
+++ create_index_for_day.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-DATE_TODAY=`date -d '+%Y-%m-%d'` 
+DATE_TODAY=`date '+%Y-%m-%d'` 
 DATE_TOMORROW=`date -d '+1 day' '+%Y-%m-%d'` 
 DATE_INSQL_N=`date -d '+2 day' '+%Y-%m-%d'`
-DATE_INNAME_TODAY=`date -d '+%Y_%m_%d'` 
+DATE_INNAME_TODAY=`date '+%Y_%m_%d'` 
 DATE_INNAME=`date -d '+1 day' '+%Y_%m_%d'` 
+echo "create table"
 psql -d hawq-recommend -c  "create table k8s_prometheus_metrics_d_$DATE_INNAME
     (check (sample_time >= date '$DATE_TOMORROW' and sample_time <= date '$DATE_INSQL_N'))
     inherits (k8s_prometheus_metrics);"
+echo "create index"
 psql -d hawq-recommend -c "
 create index k8s_prometheus_metrics_sample_time_$DATE_INNAME on k8s_prometheus_metrics using btree (sample_time);
 CREATE OR REPLACE FUNCTION k8s_prometheus_insert_trigger()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS \$\$
 BEGIN
     IF ( NEW.logdate >= DATE '$DATE_TODAY' AND
          NEW.logdate < DATE '$DATE_TOMORROW' ) THEN
@@ -23,5 +25,5 @@ BEGIN
     END IF;
     RETURN NULL;
 END;
-$$
+\$\$
 LANGUAGE plpgsql;"
