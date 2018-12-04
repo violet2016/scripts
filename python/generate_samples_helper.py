c03c3a7426dc5775da12fed344c60ef1cc42adcf
Violet Cheng <vcheng@pivotal.io>
2018-12-04 15:34:47 +0800

    rollback

--- python/generate_samples_helper.py
+++ python/generate_samples_helper.py
@@ -26,11 +26,10 @@ def create_new_query_sample(all_lists, db_connection):
     with db_connection.cursor() as cur:
         for query_id, query_info in all_lists.items():
             try:
-                if query_info['cluster'] is None:
-                    continue
+                if query_info['cluster'] is not None:
+                    insert_query_sql = 'insert into exp_queries (query_id, cluster) values (\'%s\', \'%s\')' % (query_id, query_info['cluster'])
+                    cur.execute(insert_query_sql)
                 exec_time = calc_time_delta(query_info['start_time'], query_info['end_time'])
-                insert_query_sql = 'insert into exp_queries (query_id) values (\'%s\')' % (query_id)
-                cur.execute(insert_query_sql)
                 if query_info['start_time'] is not None:
                     update_query_sql = 'update exp_queries set start_time = timestamp with time zone \'%s\' where query_id = \'%s\'' % (query_info['start_time'], query_id)
                     cur.execute(update_query_sql)
@@ -47,6 +46,7 @@ def create_new_query_sample(all_lists, db_connection):
                 db_connection.commit()
             except (Exception, psycopg2.DatabaseError) as error:
                 print('error happened', error)
+                cur.rollback()
                 continue
             except:
                 continue
